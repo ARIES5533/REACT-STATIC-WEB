baa4740eb8432d40d123fac022dc4443b6f40e11
MOSES OLUMOKO <OLUMOKOMOSES94@gmail.com>
2022-10-07 01:15:57 +0800

    first-commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome Tosin
         </a>
       </header>
     </div>
